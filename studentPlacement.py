f771929a10a38a03618a7ee3833011503d6fec74
huzaifazia17 <ziahuzaifa123@gmail.com>
2022-11-19 21:53:15 -0500

    read from file

---
+++ studentPlacement.py
@@ -0,0 +1,102 @@
+schoolPreferences = "school-preferences.txt"
+studentPreferences = "student-preferences.txt"
+
+
+def readPreferences(filename):
+
+    preferences = {}
+    with open(filename) as f:
+        keys = f.readline().split(",")
+        for line in f:
+            line = line[:-1]  # Remove \n from end of line
+            values = line.split(",")
+            for i in range(len(keys)):
+                key = keys[i]
+                value = values[i]
+                if key in preferences:
+                    preferences[key].append(value)
+                else:
+                    preferences[key] = [value]
+    return preferences
+
+
+# def findBestMatching(studentPreferences, schoolPreferences):
+#     """Finds the best matching of students to schools.
+
+#     Parameters:
+#         studentPreferences: a dictionary mapping students to lists of schools
+#         schoolPreferences: a dictionary mapping schools to lists of students
+
+#     Returns:
+#         A dictionary mapping students to schools
+#     """
+#     matching = {}
+#     for student in studentPreferences:
+#         for school in getSchoolPreferences(student, studentPreferences, schoolPreferences):
+#             if school not in matching:
+#                 matching[student] = school
+#                 break
+#     return matching
+
+
+# def getSchoolPreferences(student, studentPreferences, schoolPreferences):
+#     """Returns a list of schools in order of preference for a student.
+
+#     Parameters:
+#         student: the name of the student
+#         studentPreferences: a dictionary mapping students to lists of schools
+#         schoolPreferences: a dictionary mapping schools to lists of students
+
+#     Returns:
+#         A list of schools in order of preference for the student
+#     """
+#     schools = []
+#     for school in studentPreferences[student]:
+#         if student in schoolPreferences[school]:
+#             schools.append(school)
+#     return schools
+
+# Doing the following to remove \n from last key in dictionary because i could not find any other way to do it
+schoolPref = readPreferences(schoolPreferences)
+schoolPref["UofW"] = schoolPref["UofW\n"]
+schoolPref.pop("UofW\n")
+studentPref = readPreferences(studentPreferences)
+studentPref["Alex"] = studentPref["Alex\n"]
+studentPref.pop("Alex\n")
+# print(schoolPref)
+#print(" ")
+#print(" ")
+# print(studentPref)
+
+
+# Check if students first choice is available, if not check if some shcool has that student as first choice, else iterate
+
+
+def findBestMatching(studentPref, schoolPref):
+    studentsList = list(studentPref.keys())
+    schoolList = list(schoolPref.keys())
+    # print(schoolList)
+    matchingList = {}
+    i = 0
+    for student, studentchoice in studentPref.items():
+        print(studentchoice)
+        print("ya3nee")
+        # if studentchoice[i] in schoolList:
+        print("working0")
+        if (studentchoice[i] in schoolList):
+            matchingList[student] = studentchoice[i]
+            schoolList.remove(studentchoice[i])
+            print("working1")
+        elif (studentchoice[i] not in schoolList):
+            print("working2")
+            for school, schoolchoice in schoolPref.items():
+                print("working3")
+                if (student == schoolchoice[i] and studentchoice[i] in schoolList):
+                    print("working4")
+                    matchingList[student] = school
+                    schoolList.remove(school)
+
+    return matchingList
+
+
+print("Matching: ", findBestMatching(studentPref, schoolPref))
