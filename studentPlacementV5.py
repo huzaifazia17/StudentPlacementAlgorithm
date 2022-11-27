96e72964b54c33c3f99dc853f5a1da26b9638780
Ahmaad Ansari <88805493+ahmaad-ansari@users.noreply.github.com>
2022-11-27 16:49:19 -0500

    Commit

--- studentPlacementV5.py
+++ studentPlacementV5.py
@@ -1,12 +1,10 @@
-import copy
-
 def createDictionary(filename):
     dictionary = {}
     with open(filename) as f:
         keys = f.readline().split(",")
         keys[-1] = keys[-1].replace("\n", "")   # Remove \n from end of line
         for line in f:
-            line = line[:-1]  # Remove \n from end of line
+            line = line[:-1]  # Removes \n from end of line
             values = line.split(",")
             for i in range(len(keys)):
                 key = keys[i]
@@ -19,24 +17,7 @@ def createDictionary(filename):
 
 def printDictionary(dictionary):
     for key, value in dictionary.items() :
-        print(key, "-->",value)
-
-def createEmptyDictionaryFromKeyList(keyList):
-    # Initialize dictionary
-    dictionary = {}
-    
-    # Iterating through the elements of list
-    for i in keyList:
-        dictionary[i] = ''
-    return dictionary
-
-def deleteValue(dictionary, lookUp):
-    for key, valueList in dictionary.items():
-        for i in range (0, len(valueList)):
-            if (valueList[i] == lookUp):
-                valueList[i] = 'XXX'
-                dictionary[key] = valueList
-    return dictionary
+        print(key, "is matched with",value)
 
 def removeValueFromDictionary(dictionary, value):
     for key, valueList in dictionary.items():
@@ -44,61 +25,54 @@ def removeValueFromDictionary(dictionary, value):
         dictionary[key] = valueList
     return dictionary
 
-def run():
+def decisionMakingAlgorithm():
+    # Define global variables and regular variables
     global universityPref
     global studentPref
-    global studentPrefTEMP
-    global universityPrefTEMP
     global availableStudents
     global availableUniversities
     global decisions
+    currStudentIndex = 0
 
-    for i in range (0, len(studentPref.keys())):
-        for university in universityPref.keys():
-            if(i < len(availableStudents)):
-                currStudent = universityPref[university][i]
-                if((currStudent in availableStudents) and (university in availableUniversities)):
+    while(len(availableStudents) > 0):  # Runs the program as long as there are still matches to be made
+        resetLoop = False
+        
+        for university in universityPref.keys():    # Loops through all universities in the university preference dictionary
+            
+            if(currStudentIndex < len(availableStudents)):  # Checks if the current index is out of bonunds                
+                currStudent = universityPref[university][currStudentIndex]  # Sets the current tsudent to be matched
+                
+                if((currStudent in availableStudents) and (university in availableUniversities)):   # If the student and university are both available for matches
+                    
                     if(studentPref[currStudent][0] == university):
-
-                        decisions[currStudent] = university
+                        decisions[currStudent] = university # Remove all instances of the student and university from both dictionaries
                         
                         # Remove all instances of the student and university from both dictionaries
                         availableStudents.remove(currStudent)
                         availableUniversities.remove(university)
-
                         studentPref = removeValueFromDictionary(studentPref, university)
                         universityPref = removeValueFromDictionary(universityPref, currStudent)
+                        
+                        resetLoop = True    # Reset the loop and indices
+        
+        if (resetLoop):
+            currStudentIndex = 0
+        else:
+            currStudentIndex += 1
 
-                        # studentPrefTEMP = deleteValue(studentPrefTEMP, university)
-                        # universityPrefTEMP = deleteValue(universityPrefTEMP, currStudent)
-                        # del studentPrefTEMP[currStudent]
-                        # del universityPrefTEMP[university]
-
-                        # print()
-                        # print()
-                        # printDictionary(studentPrefTEMP)
-                        # printDictionary(studentPref)
-                        # print()
-                        # printDictionary(universityPrefTEMP)
-                        # printDictionary(universityPref)
-                        # print()
-                        # print()
-                        run()
-                        # university = availableUniversities[0]
 
 # Passes filenames to function which returns the generated dictionary
 universityPref = createDictionary("school-preferences.txt")
 studentPref = createDictionary("student-preferences.txt")
 
-studentPrefTEMP = copy.deepcopy(studentPref)
-universityPrefTEMP = copy.deepcopy(universityPref)
-
+# Creates lists using the keys from a dictionary
 availableStudents = list(studentPref.keys())
 availableUniversities = list(universityPref.keys())
 
-decisions = createEmptyDictionaryFromKeyList(availableStudents)
+# Creates an empty dictionary with an associated list
+decisions = dict(zip(availableStudents, [None]*len(availableStudents)))
 
-run()
+decisionMakingAlgorithm()
 
 printDictionary(decisions)
 
